8f5d9c08ec618bcc66bcbdb95fd08e4f49442716
Luis Fernando Silva de Oliveira <c1299072@INTRABB.BB.COM.BR>
2019-01-02 16:52:57 -0200

    Alterando o JavaScript da aplicacao

--- controller/UserController.js
+++ controller/UserController.js
@@ -8,32 +8,106 @@ class UserController {
         this.onSubmit();
     }
 
+    // Pega o evento de submit do botão 
     onSubmit() {
 
         this.formEl.addEventListener("submit", event => {
 
             event.preventDefault();
 
-            this.addLine(this.getValues());
-           
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
+            let values = this.getValues();
+
+            if (!values) return false;
+
+            this.getPhoto().then(
+                    (content) => {
+
+                        values.photo = content;
+
+                        this.addLine(values);
+
+                        this.formEl.reset();
+
+                        btn.disabled = false;
+
+                },  (e) => {
+                        console.error(e);
+                }
+            );
+
         });
     }
 
+    // Função que renderiza a foto e a coloca na tela dinamicamente sem salvar no banco de dados  
+    getPhoto() {
+
+        return new Promise((resolve, reject) => {
+
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item => {
+
+            if (item.name === 'photo') {
+                return item;
+            }
+        });
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = () => {
+
+            resolve(fileReader.result);
+
+        };
+
+        fileReader.onerror = (e) => {
+            reject(e);
+        }
+
+        if (file) {
+            fileReader.readAsDataURL(file);
+        } else {
+            resolve('dist/img/boxed-bg.jpg')
+        }
+    })
+}
+
+    // Pega os valores dos campos
     getValues() {
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function (field, index){
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
 
             if (field.name == "gender") {
 
                 if(field.checked){                
                     user[field.name] = field.value;
                 }
+            } else if (field.name == "admin") {
+            
+                user[field.name] = field.checked;
+
             } else {
-                user[field.name] = field.value
+
+                user[field.name] = field.value;
             }
         });
+
+        if (!isValid) {
+            return false;
+        }
 
         return new User(
             user.name, 
@@ -48,22 +122,48 @@ class UserController {
 
     }
 
-    addLine(dataUser){
+    // Função que insere os HTMLS na tela
+    addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
+        let tr = document.createElement('tr');
+
+        tr.dataset.user = JSON.stringify(dataUser);
+
+        tr.innerHTML = `
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
         `;
     
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    // Faz a contagem de Users e Admin e atualiza a Dashboard
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr => {
+
+            numberUsers ++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) numberAdmin ++;
+
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
     }
 
 }
